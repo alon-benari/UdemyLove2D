ccc56c3377175ec8b6112ff7c871a75259e578b6
alon-benari <alon.benari@gmail.com>
2024-12-05 20:07:20 -0800

    update

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+message = 5
+condition = -25
+
+if condition >0  then
+    message = 1
+else 
+    message = 2
+end 
+
 function love.draw()
-    love.graphics.print("Hello there")
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message)
 end
